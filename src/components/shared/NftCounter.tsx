37d615e90be3b67224413df6f1a44a281cc8f456
Marcin <zolkiewski.marcin@gmail.com>
2021-10-26 21:16:46 +0200

    handle state init better

--- src/components/shared/NftCounter.tsx
+++ src/components/shared/NftCounter.tsx
@@ -7,6 +7,10 @@ const { Text } = Typography;
 export default function NftCounter() {
   const { itemsLeft } = useDeepWaifuContract();
 
+  if (itemsLeft < 0) {
+    return <div />;
+  }
+
   return (
     <Text className="text14">
       Hurry up, there's only <strong> {itemsLeft} NFTs left to mint!</strong>
--- src/contexts/DeepWaifuContract.tsx
+++ src/contexts/DeepWaifuContract.tsx
@@ -5,6 +5,7 @@ import deepWaifuAbi from '../abi/DeepWaifu.json';
 import { DeepWaifu } from '../abi/typechain';
 import { NFT_CONTRACT_ADDRESS } from '../env';
 import { WalletContext } from './Wallet';
+import { MINUTE_MILLIS } from '../constants';
 
 interface IPaymentResult {
   tx: string;
@@ -26,8 +27,8 @@ interface IProps {
 // const deepWaifuInterface = new ethers.utils.Interface(deepWaifuAbi.abi);
 
 export const DeepWaifuContractContext = createContext<IDeepWaifuContractContext>({
-  maxItems: 0,
-  itemsLeft: 0,
+  maxItems: -1,
+  itemsLeft: -1,
   priceWei: ethers.BigNumber.from(0),
   onPayForMint: () => Promise.resolve({} as any),
 });
@@ -35,8 +36,8 @@ export const DeepWaifuContractContext = createContext<IDeepWaifuContractContext>
 export const DeepWaifuContractProvider: React.FC<IProps> = ({ children }: IProps) => {
   const { provider, signer } = useContext(WalletContext);
   const [contract, setContract] = useState<DeepWaifu>();
-  const [maxItems, setMaxItems] = useState(0);
-  const [itemsLeft, setItemsLeft] = useState(0);
+  const [maxItems, setMaxItems] = useState(-1);
+  const [itemsLeft, setItemsLeft] = useState(-1);
   const [priceWei, setPriceWei] = useState(ethers.BigNumber.from(0));
 
   const handlePayForMint = useCallback(async (): Promise<IPaymentResult> => {
@@ -64,71 +65,52 @@ export const DeepWaifuContractProvider: React.FC<IProps> = ({ children }: IProps
   }, [provider, signer]);
 
   useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
     if (contract) {
       fetchMaxItems();
+      intervalId = setInterval(fetchMaxItems, 5 * MINUTE_MILLIS);
     }
 
     async function fetchMaxItems() {
       const res = await contract!.maxItems();
       setMaxItems(res);
     }
+
+    return () => clearInterval(intervalId);
   }, [contract]);
 
   useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
     if (contract) {
       fetchItemsLeft();
+      intervalId = setInterval(fetchItemsLeft, 1 * MINUTE_MILLIS);
     }
 
     async function fetchItemsLeft() {
       const res = await contract!.currentId();
       setItemsLeft(maxItems - res.toNumber());
     }
+
+    return () => clearInterval(intervalId);
   }, [contract, maxItems]);
 
   useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
     if (contract) {
       fetchPriceWei();
+      intervalId = setInterval(fetchPriceWei, 5 * MINUTE_MILLIS);
     }
 
     async function fetchPriceWei() {
       const res = await contract!.mintPrice();
       setPriceWei(res);
     }
+
+    return () => clearInterval(intervalId);
   }, [contract]);
-
-  useEffect(() => {
-    // accessList: null
-    // blockHash: null
-    // blockNumber: null
-    // chainId: 4369
-    // confirmations: 0
-    // creates: null
-    // data: "0x68c81ee8"
-    // from: "0xA7293D776c5f5Aa450e9592E29E22683ffC39B67"
-    // gasLimit: BigNumber {_hex: '0xf78f', _isBigNumber: true}
-    // gasPrice: BigNumber {_hex: '0x3b9aca00', _isBigNumber: true}
-    // hash: "0x1c783ec162fdf463473dc7444ea9a55353977d58d4019f7c55132ff78ef6c153"
-    // nonce: 6
-    // r: "0x7f51c1b683bbd2fc763ba51e37735f186f965e62c9943d015cf7c806c744aaa2"
-    // s: "0x54b96f67e6dc13501db504eaafcf9d028117e70e7fcade67d5ed081ea0a392a1"
-    // to: "0x114a77E482d6B1b8730894E0BF8586a1aB2EE7D6"
-    // transactionIndex: null
-    // type: 0
-    // v: 8774
-    // value: BigNumber {_hex: '0x01a055690d9db80000', _isBigNumber: true}
-    // wait: confirmations => {…}
-
-    async function lol() {
-      const tx = await provider.getTransaction('0x1c783ec162fdf463473dc7444ea9a55353977d58d4019f7c55132ff78ef6c153');
-      console.log(tx);
-      const txRec = await provider.getTransactionReceipt(
-        '0x1c783ec162fdf463473dc7444ea9a55353977d58d4019f7c55132ff78ef6c153'
-      );
-      console.log(txRec);
-    }
-
-    lol();
-  }, []);
 
   return (
     <DeepWaifuContractContext.Provider
